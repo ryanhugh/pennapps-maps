50eab0693b66c4c1a30cf710e4d01c0d13d9b464
rysquash <rysquash@gmail.com>
2016-01-23 00:11:44 -0500

    added model that appears on start and asks for a zip code

--- ZipPopup.js
+++ ZipPopup.js
@@ -1,13 +1,25 @@
 'use strict';
 
 
-function ZipPopup () {
-	
+function ZipPopup() {
+
+
+
+	$(function () {
+
+		this.zipCodeTextBox = document.getElementById('zipCodeTextBoxId')
+	}.bind(this))
+
 }
 
 
 
+ZipPopup.prototype.go = function () {
+	console.log('User entered:',this.zipCodeTextBox.value);
 
+$('#myModal').modal('toggle')
+
+};
 
 
 window.zipPopup = new ZipPopup();
