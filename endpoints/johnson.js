71b366a048899a377cecc58fe6a2f71e63ab18e1
rysquash <rysquash@gmail.com>
2016-01-23 16:57:12 -0500

    johnson getter for average foods

--- endpoints/johnson.js
+++ endpoints/johnson.js
@@ -33,14 +33,14 @@ Johnson.prototype.fire = function (callback) {
 
 			return callback(err)
 		}
-		return callback(null, JSON.parse(xmlhttp.response).bloodGlucose)
+		return callback(null, JSON.parse(xmlhttp.response))
 
 
 	}.bind(this)
 
 
-	xmlhttp.open('GET', 'https://jnj-dev.apigee.net/otr/v1/patient/-/healthdata/search?type=bloodGlucose&startDate=2015-10-26T00%3A00%3A00&endDate=2016-01-24T00%3A00%3A00&limit=20', true);
-	xmlhttp.setRequestHeader('Authorization', 'Bearer UIjrFjyIAWr4Lv60R4lsUE983vpq')
+	xmlhttp.open('GET', 'https://jnj-dev.apigee.net/otr/v1/patient/-/healthdata/search?type=bloodGlucose,food&startDate=2010-10-26T00%3A00%3A00&endDate=2017-01-24T00%3A00%3A00&limit=2000', true);
+	xmlhttp.setRequestHeader('Authorization', 'Bearer TxTLwxaAmKAGyAGEqjbUOBUPiWlm')
 	xmlhttp.send();
 };
 
@@ -51,13 +51,15 @@ Johnson.prototype.go = function (callback) {
 			return callback(err)
 		}
 
+		response = response.bloodGlucose;
+
 		//loop through responce and create location data
 
 		response.forEach(function (item) {
 			if (!item.location_coordinates) {
 				item.location_coordinates = [{
-					"latitude": 39.955+(Math.random()*.008-.004),
-					"longitude": -75.19+(Math.random()*.008-.004)
+					"latitude": 39.955 + (Math.random() * .008 - .004),
+					"longitude": -75.19 + (Math.random() * .008 - .004)
 				}]
 			};
 
@@ -66,7 +68,7 @@ Johnson.prototype.go = function (callback) {
 			};
 
 
-			if (item.bgValue.value>70 && item.bgValue.value<140) {
+			if (item.bgValue.value > 70 && item.bgValue.value < 140) {
 				item.title = 'Good level'
 			}
 			else {
@@ -74,7 +76,7 @@ Johnson.prototype.go = function (callback) {
 			}
 
 			if (item.mealTag) {
-				item.location_name 	= 'Before food'
+				item.location_name = 'Before food'
 			}
 			else {
 				item.location_name = 'After food'
@@ -82,27 +84,63 @@ Johnson.prototype.go = function (callback) {
 
 
 			//coffee 
-			var foods = ['coffee','crackers','candy','turkey','cheesecake','ice cream'];
+			var foods = ['coffee', 'crackers', 'candy', 'turkey', 'cheesecake', 'ice cream'];
 
-			item.food = foods[parseInt(Math.random()*foods.length)]
+			item.food = foods[parseInt(Math.random() * foods.length)]
 
-			item.stepCount = Math.random()*4000+1000
-
-
+			item.stepCount = Math.random() * 4000 + 1000
 
 
 
 		}.bind(this))
-		return callback(null,response)
+		return callback(null, response)
 
 
 	}.bind(this))
 };
 
 
+
+Johnson.prototype.average = function (callback) {
+	this.fire(function (err, results) {
+		if (err) {
+			return callback(err)
+		};
+		var food = results.food;
+
+		var days = {};
+
+		food.forEach(function (aFood) {
+
+			var day = new Date(aFood.createdDate).getDay();
+
+			if (!days[day]) {
+				days[day] = {
+					count:0,
+					sum:0
+				}
+			}
+
+			days[day].count+=1;
+			days[day].sum += aFood.carbohydrates.value;
+		}.bind(this))
+
+		for (var day in days) {
+			days[day] = days[day].sum/days[day].count;
+		}
+
+
+		return callback(null, days)
+
+
+	}.bind(this))
+};
+
+
+
 var instance = new Johnson()
 
-instance.fire(function (err, response) {
+instance.average(function (err, response) {
 	console.log("err", err, response);
 }.bind(this))
 
