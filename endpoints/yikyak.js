82a61984993270d8cd39dbad7e7afccb6cc74909
rysquash <rysquash@gmail.com>
2016-01-23 03:31:23 -0500

    server serves static files, yikyak works

--- endpoints/yikyak.js
+++ endpoints/yikyak.js
@@ -2,18 +2,17 @@
 
 
 
-
-function YikYak () {
+function YikYak() {
 	this.url = 'http://162.243.4.193/get'
 }
 
 
-YikYak.prototype.go = function(callback) {
-	
+YikYak.prototype.fire = function (config, callback) {
+
 
 	var xmlhttp = new XMLHttpRequest();
 	xmlhttp.onreadystatechange = function () {
-		
+
 
 		if (xmlhttp.readyState != XMLHttpRequest.DONE) {
 			return;
@@ -35,15 +34,54 @@ YikYak.prototype.go = function(callback) {
 
 			return callback(err)
 		}
-		return callback(null,JSON.parse(xmlhttp.response))
+		return callback(null, JSON.parse(xmlhttp.response))
 
 
 	}.bind(this)
 
 
-	xmlhttp.open('GET', this.url, true);
+	xmlhttp.open('GET', this.url+'/'+config.lat+'/'+config.long, true);
 	xmlhttp.send();
 };
+
+YikYak.prototype.go = function (callback) {
+	if (!navigator.geolocation) {
+		return callback('need chrome')
+	};
+
+	//get locations
+	navigator.geolocation.getCurrentPosition(function (position) {
+
+		//get yik yak data
+		this.fire({
+			lat: position.coords.latitude,
+			long: position.coords.longitude
+		}, function (err, results) {
+
+
+			//now convert to everbright schema
+			results.forEach(function (post) {
+				post.title = post.message
+
+				post.location_coordinates = [{
+					"latitude": post.latitude,
+					"longitude": post.longitude
+				}]
+
+				post.comment_count = post.comments
+
+			}.bind(this))
+
+
+			callback(null,results)
+
+
+		}.bind(this))
+	}.bind(this));
+
+
+};
+
 
 var instance = new YikYak();
 
@@ -51,6 +89,6 @@ window.yikYak = instance
 
 
 
-instance.go(function (err,response) {
-	console.log(err,response);
+instance.go(function (err, response) {
+	console.log(err, response);
 }.bind(this))
