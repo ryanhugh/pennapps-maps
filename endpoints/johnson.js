77d99fc1595bf0950a9dd9bcf6cc1d67a8d28cb8
rysquash <rysquash@gmail.com>
2016-01-23 22:22:14 -0500

    merged jnj data

--- endpoints/johnson.js
+++ endpoints/johnson.js
@@ -5,8 +5,7 @@ function Johnson() {
 }
 
 
-
-Johnson.prototype.fire = function (callback) {
+Johnson.prototype.request = function (config, callback) {
 
 
 	var xmlhttp = new XMLHttpRequest();
@@ -35,13 +34,31 @@ Johnson.prototype.fire = function (callback) {
 		}
 		return callback(null, JSON.parse(xmlhttp.response))
 
-
 	}.bind(this)
 
+	if (config.headers) {
+		for (var attrName in config.headers) {
+			xmlhttp.setRequestHeader(attrName, config.headers[attrName])
 
-	xmlhttp.open('GET', 'https://jnj-dev.apigee.net/otr/v1/patient/-/healthdata/search?type=bloodGlucose,food&startDate=2010-10-26T00%3A00%3A00&endDate=2017-01-24T00%3A00%3A00&limit=2000', true);
-	xmlhttp.setRequestHeader('Authorization', 'Bearer TxTLwxaAmKAGyAGEqjbUOBUPiWlm')
+		}
+	};
+
+	xmlhttp.open('GET', config.url, true);
 	xmlhttp.send();
+
+};
+
+
+Johnson.prototype.fire = function (callback) {
+
+	this.request({
+		url: 'https://jnj-dev.apigee.net/otr/v1/patient/-/healthdata/search?type=bloodGlucose,food&startDate=2010-10-26T00%3A00%3A00&endDate=2017-01-24T00%3A00%3A00&limit=2000',
+		headers: {
+			'Authorization': 'Bearer TxTLwxaAmKAGyAGEqjbUOBUPiWlm'
+		}
+	}, callback)
+
+
 };
 
 
@@ -116,17 +133,17 @@ Johnson.prototype.average = function (callback) {
 
 			if (!days[day]) {
 				days[day] = {
-					count:0,
-					sum:0
+					count: 0,
+					sum: 0
 				}
 			}
 
-			days[day].count+=1;
+			days[day].count += 1;
 			days[day].sum += aFood.carbohydrates.value;
 		}.bind(this))
 
 		for (var day in days) {
-			days[day] = days[day].sum/days[day].count;
+			days[day] = days[day].sum / days[day].count;
 		}
 
 
@@ -137,10 +154,59 @@ Johnson.prototype.average = function (callback) {
 };
 
 
+Johnson.prototype.mergedObjects = function (callback) {
+
+	this.request({
+		url: 'examples/johnson%20and%20johnson/data.js',
+	}, function (err, results) {
+		if (err) {
+			return callback(err)
+		};
+
+
+		//now group the data by the jawns
+
+
+		var output = [];
+		for (var i = 0; i < 20; i++) {
+			var newObj = {
+				id: Math.random() + ' ' + Math.random()
+			}
+
+			//pull a random data
+			var index = parseInt(results.bloodGlucose.length * Math.random())
+			newObj.bloodGlucose = results.bloodGlucose[index].bgValue
+
+
+
+			index = parseInt(results.food.length * Math.random())
+			newObj.food = results.food[index].carbohydrates
+
+
+
+			index = parseInt(results.exercise.length * Math.random())
+			newObj.exercise = results.exercise[index].exercise
+
+
+
+			index = parseInt(results.bolusInsulin.length * Math.random())
+			newObj.bolusInsulin = results.bolusInsulin[index].bolusDelivered
+
+			output.push(newObj)
+		};
+		return callback(null,output)
+
+
+	}.bind(this))
+
+
+};
+
+
 
 var instance = new Johnson()
 
-instance.average(function (err, response) {
+instance.mergedObjects(function (err, response) {
 	console.log("err", err, response);
 }.bind(this))
 
